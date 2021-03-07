c04bf2a6cd4df457af79672a833e399d9a0593ce
wjfskr <1216631526@qq.com>
2021-03-07 15:14:48 +0800

    加入显示天气信息功能

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.coolweather.android;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -81,6 +82,13 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel == LEVEL_CITY) {//当前选中的级别==市
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }
+                else if (currentLevel == LEVEL_COUNTY){//是县
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);//启动WeatherActivity
+                    intent.putExtra("weather_id", weatherId);//传值P50
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,10 @@
+package com.coolweather.android.gson;
+
+public class Basic {
+    public String cityName;//城市名
+    public String weatherId;//城市对应的天气id
+    public Update update;
+    public class Update{
+        public String updateTime;//天气更新时间
+    }
+}
